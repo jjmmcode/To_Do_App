802b48346a69dcb6fed13a0413a4a84a08559529
jjmmcode <joeljesus30030@gmail.com>
2025-07-22 23:21:57 -0400

    chore: incluir carpeta Migrations al repositorio

--- backend/Program.cs
+++ backend/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=tasks.db"));//
+    options.UseSqlite("Data Source=tasks.db"));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
